30dd9b61f8340bb7407972d70c2feb05602b4715
Juyoung <juyojeon@student.42seoul.kr>
2023-09-19 21:41:41 +0900

    Revert "[Edit] maybe sprite finish?"
    This reverts commit 36d06e3edfe803fff6d7e6d95090e67dc12ef9b0.

--- bonus/sprite_bonus.c
+++ bonus/sprite_bonus.c
@@ -6,26 +6,23 @@
 /*   By: juyojeon <juyojeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 21:15:27 by juyojeon          #+#    #+#             */
-/*   Updated: 2023/09/19 16:33:41 by juyojeon         ###   ########.fr       */
+/*   Updated: 2023/09/18 23:56:57 by juyojeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d_bonus.h"
 
 static void	draw_sprite_img(t_data *data, t_img *s_img, t_sprite *sprite);
-static void	set_tex_sprite(t_info *tex, t_sprite *spr, t_img *img);
-static void	texture_color(t_info *tex, t_sprite *sprite, \
-unsigned int *dst, t_img *s_img);
+static void set_tex_sprite(t_texture *tex, t_sprite *spr, int row, t_img *img);
+static void	texture_color(t_texture *tex, t_sprite *sprite, unsigned int *dst);
+
 typedef struct s_info
 {
 	int	line_height;
-	int	line_width;
-	int	row_idx;
-	int	end_idx;
-	int	step_x;
 	int	pos_x;
-	int	step_y;
 	int	pos_y;
+	int	step_x;
+	int	step_y;
 }	t_info;
 
 void	draw_sprite(t_data *data)
@@ -48,63 +45,44 @@ void	draw_sprite(t_data *data)
 
 static void	draw_sprite_img(t_data *data, t_img *s_img, t_sprite *sprite)
 {
-	t_info	tex;
-	int		i;
-	char	*dst;
+	// t_info		tex;
+	// int			row_idx;
+	// int			end_idx;
+	// int			i;
+	// char		*dst;
 
-	set_tex_sprite(&tex, sprite, s_img);
-	while (tex.row_idx <= tex.end_idx)
-	{
-		i = sprite->start - 1;
-		tex.pos_x = (sprite->start + (tex.line_width - WINDOW_WIDTH) / 2) \
-		* tex.step_x;
-		while (++i <= sprite->end)
-		{
-			dst = data->img.addr + tex.row_idx * data->img.size_l \
-					+ i * (data->img.bpp / 8);
-			texture_color(&tex, sprite, (unsigned int *)dst, s_img);
-			tex.pos_x += tex.step_x;
-		}
-		tex.row_idx++;
-		tex.pos_y += tex.step_y;
-	}
+	// tex.line_height = (int)(WINDOW_HEIGHT / sprite->dist);
+	// row_idx = (WINDOW_HEIGHT - tex.line_height) / 2;
+	// end_idx = (WINDOW_HEIGHT + tex.line_height) / 2;
+	// set_tex_sprite(&tex, sprite, row_idx, s_img);
+	// if (row_idx < 0)
+	// 	row_idx = 0;
+	// if (end_idx >= WINDOW_HEIGHT)
+	// 	end_idx = WINDOW_HEIGHT - 1;
+	// while (row_idx <= end_idx)
+	// {
+	// 	i = sprite->start - 1;
+	// 	while (++i <= sprite->end)
+	// 	{
+			
+	// 		dst = data->img.addr + row_idx * data->img.size_l \
+	// 				+ i * (data->img.bpp / 8);
+	// 		texture_color(&tex, sprite, (unsigned int *)dst);
+	// 	}
+	// 	row_idx++;
+	// 	tex.pos += tex.step;
+	// }
 }
 
-static void	set_tex_sprite(t_info *tex, t_sprite *spr, t_img *img)
-{
-	tex->line_height = (int)(WINDOW_HEIGHT / spr->dist);
-	tex->line_width = (int)(WINDOW_WIDTH / spr->dist);
-	tex->row_idx = (WINDOW_HEIGHT - tex->line_height) / 2;
-	if (tex->row_idx < 0)
-		tex->row_idx = 0;
-	tex->end_idx = (WINDOW_HEIGHT + tex->line_height) / 2;
-	if (tex->end_idx >= WINDOW_HEIGHT)
-		tex->end_idx = WINDOW_HEIGHT - 1;
-	tex->step_x = img->height / (double)(tex->line_width);
-	tex->pos_x = (spr->start + (tex->line_width - WINDOW_WIDTH) / 2) \
-	* tex->step_x;
-	tex->step_y = img->height / (double)(tex->line_height);
-	tex->pos_y = (tex->row_idx + (tex->line_height - WINDOW_HEIGHT) / 2) \
-	* tex->step_y;
+// static void	set_tex_sprite(t_texture *tex, t_sprite *spr, int row, t_img *img)
+// {
 
-}
+// }
 
-static void	texture_color(t_info *tex, t_sprite *sprite, \
-unsigned int *dst, t_img *s_img)
-{
-	int		tex_x;
-	int		tex_y;
-	char	*tex_color;
+// static void	texture_color(t_texture *tex, t_sprite *sprite, unsigned int *dst)
+// {
+// 	int		tex_x;
+// 	int		tex_y;
 
-	if ((int)(tex->pos_x) >= s_img->width)
-		tex_x = s_img->width - 1;
-	else
-		tex_x = (int)(tex->pos_x);
-	if ((int)(tex->pos_y) >= s_img->height)
-		tex_y = s_img->height - 1;
-	else
-		tex_y = (int)(tex->pos_y);
-	tex_color = s_img->addr + tex_y * s_img->size_l \
-				+ tex_x * (s_img->bpp / 8);
-	dst = (unsigned int *)tex_color;
-}
+// 	tex_x = (int)(tex->pos) & (tex->curr_img->width - 1);
+// }
